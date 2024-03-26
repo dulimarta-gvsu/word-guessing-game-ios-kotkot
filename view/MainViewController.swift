a8ed29b4f9534657f8c99c31fea812bba3611305
Alec Mirambeau <alecmirambeau@gmail.com>
2024-03-26 17:19:29 -0400

    added changes from class

--- view/MainViewController.swift
+++ view/MainViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    var correctCount: UILabel!
+    var checkButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Change the background color to your preference
         self.view.backgroundColor = .lightGray
+        correctCount = view.viewWithTag(21) as? UILabel
+        checkButton = view.viewWithTag(51) as? UIButton
+        
+        checkButton.addTarget(self, action: #selector(doSome), for: UIControl.Event.touchUpInside)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func doSome(){
+        correctCount.text = "You got it"
     }
 
 
