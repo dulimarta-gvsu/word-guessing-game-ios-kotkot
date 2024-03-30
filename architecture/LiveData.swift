c56b08a57e7e9ae976fd8314e1b8e541339f88a0
Alec Mirambeau <alecmirambeau@gmail.com>
2024-03-30 11:37:13 -0400

    added liveData class

---
+++ architecture/LiveData.swift
@@ -0,0 +1,50 @@
+//
+//  architecture.swift
+//  word_guess
+//
+//  Created by Alec Mirambeau on 3/30/24.
+//
+
+import Foundation
+
+typealias ObserverFunc<T> = (T?) -> Void
+
+class LiveData<T> {
+    private var _value: T?
+    private var subscribers: Array<ObserverFunc<T>> = []
+
+    init(_ value: T? = nil) {
+        self._value = value
+    }
+    
+    // Declare a property with setter and getter
+    var value: T? {
+        set(newValue) {
+            self._value = newValue
+            for fn in self.subscribers {
+                // Notify each subscriber of the new value
+                fn(newValue)
+            }
+        }
+        get {
+            return self._value
+        }
+    }
+    
+    /* must use post() when updating from a non-UI thread */
+    func post(newValue: T?) {
+        self._value = newValue
+        DispatchQueue.main.async {
+            for fn in self.subscribers {
+                // Notify each subscriber of the new value
+                fn(newValue)
+            }
+
+        }
+    }
+    
+    func observe(_ obs: @escaping ObserverFunc<T>) {
+        self.subscribers.append(obs)
+        obs(self._value)
+    }
+}
