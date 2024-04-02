73952d3ab8cee8f06176ff5cd1eb55c8a635b720
Alec Mirambeau <alecmirambeau@gmail.com>
2024-04-02 17:15:38 -0400

    Changed the scene Delgate so it's back to main screen

--- word_guess/SceneDelegate.swift
+++ word_guess/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let winScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: winScene)
-        self.window?.rootViewController = UINavigationController(rootViewController: TableViewViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         self.window?.makeKeyAndVisible()
     }
 
